e77f1085a820a95ae94862bbb2648103bb7b4859
いっとく <73953416+ittoku703@users.noreply.github.com>
2022-06-14 03:06:22 +0900

    add introduction_to_HTML

--- docs/script.js
+++ docs/script.js
@@ -2,5 +2,6 @@ const pElem = document.createElement('p');
 const body = document.querySelector('body');
 
 pElem.textContent = "My name is ittoku703!";
+pElem.style.color = "green";
 
 body.appendChild(pElem);
